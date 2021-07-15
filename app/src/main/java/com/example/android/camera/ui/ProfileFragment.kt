cc659e41d3b12541f8fe1c16c0c177ee88572788
ZeyadShehata <zeyadelzek@gmail.com>
2021-07-16 01:22:02 +0200

    Feat: Add boolean livedata that is set on clicking the add imageview and implement observer for that livedata so it shows the dialog when it's value is true

--- app/src/main/java/com/example/android/camera/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/android/camera/ui/ProfileFragment.kt
@@ -1,14 +1,19 @@
 package com.example.android.camera.ui
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.android.camera.databinding.DialogOptionsBinding
 import com.example.android.camera.databinding.FragmentProfileBinding
 
 class ProfileFragment: Fragment() {
     private var _binding : FragmentProfileBinding? = null
+    private  val viewModel: ProfileViewModel by viewModels()
     private val binding
             get() = _binding!!
     override fun onCreateView(
@@ -17,6 +22,17 @@ class ProfileFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        binding.profileViewModel = viewModel
+        viewModel.addButtonClicked.observe(viewLifecycleOwner,{ clicked ->
+            if(clicked){
+                val dialog = Dialog(requireContext())
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                val dialogBinding = DialogOptionsBinding.inflate(LayoutInflater.from(dialog.context))
+                dialog.setContentView(dialogBinding.root)
+                dialog.show()
+            }
+        })
         return binding.root
     }
 
--- app/src/main/java/com/example/android/camera/ui/ProfileViewModel.kt
+++ app/src/main/java/com/example/android/camera/ui/ProfileViewModel.kt
@@ -1,4 +1,21 @@
 package com.example.android.camera.ui
 
-class ProfileViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel() : ViewModel(){
+    private  var _addButtonClicked = MutableLiveData<Boolean>()
+    val addButtonClicked : LiveData<Boolean>
+        get() = _addButtonClicked
+    fun setAddButtonClicked(){
+        _addButtonClicked.value= true
+    }
+  /*  fun makeSourceDialog(){
+        val dialog = Dialog(getApplication<Application>().applicationContext)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        val dialogBinding = DialogOptionsBinding.inflate(LayoutInflater.from(dialog.context))
+        dialog.setContentView(dialogBinding.root)
+        dialog.show()
+    }*/
 }
