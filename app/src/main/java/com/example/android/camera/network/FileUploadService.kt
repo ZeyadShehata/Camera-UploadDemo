ab452da37652803b44be70be088ba2f666ad1d26
ZeyadShehata <zeyadelzek@gmail.com>
2021-07-17 17:06:31 +0200

    Feat: implement upload post method in FileUploadService interface

--- app/src/main/java/com/example/android/camera/network/FileUploadService.kt
+++ app/src/main/java/com/example/android/camera/network/FileUploadService.kt
@@ -1,4 +1,12 @@
 package com.example.android.camera.network
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface FileUploadService {
+    @GET("upload")
+    suspend fun UploadPhoto(
+        @Query("Content-Disposition:") method: String,
+        @Query("filename") name: String
+    )
 }
